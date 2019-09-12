03dbe193be93eb011685050bf38e4f616ec49919
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-09-12 16:29:21 +0300

    Change fragment syntax rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     'no-console': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 0,
+    'react/jsx-fragments': 0,
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import { GlobalStyle } from './indexStyles';
 import App from './components/App';
 
 ReactDOM.render(
-  <>
+  <Fragment>
     <GlobalStyle />
     <App />
-  </>,
+  </Fragment>,
   document.getElementById('root')
 );
