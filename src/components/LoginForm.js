dbd646ed36576a20cecb3fbf1ffaa66e66dc8f5c
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-09-23 15:15:50 +0300

    Add units to mock api

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const LoginForm = ({ handleLogin }) => {
+  const [units, setUnits] = useState([]);
+
+  const fetchUnits = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/units');
+      const data = await response.json();
+
+      setUnits(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUnits();
+  }, []);
+
   return (
     <Formik
       initialValues={{ fazerId: '', lastName: '', unitName: '' }}
@@ -34,7 +51,11 @@ const LoginForm = ({ handleLogin }) => {
 
           <Field component="select" name="unitName">
             <option value="">Valitse toimipiste</option>
-            <option value="Lahti">Lahti</option>
+            {units.map(unit => (
+              <option key={unit.id} value={unit.name}>
+                {unit.name}
+              </option>
+            ))}
           </Field>
           <ErrorMessage name="unitName" component="div" />
 
