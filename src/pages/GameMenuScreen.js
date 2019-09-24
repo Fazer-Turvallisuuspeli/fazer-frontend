46c3deb05d542ed493ed398329569edb4c19898c
Veikko Lehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-09-24 18:45:02 +0300

    Create game and main menu

--- src/pages/GameMenuScreen.js
+++ src/pages/GameMenuScreen.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import InfoDisplay from '../components/InfoDisplay';
 
 const GameMenuScreen = () => {
+  const [instructions, setInstructions] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const fetchData = async () => {
+    const response = await fetch('/api/v1/game/info');
+    const data = await response.json();
+
+    setInstructions(data.instructions);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
-      <h1>Game Menu</h1>
+      <h1>Valitse kategoria</h1>
+
+      {isModalOpen && <InfoDisplay data={instructions} />}
+
+      <Link to="/gameMenu">
+        <button type="button" onClick={handleModalClose}>
+          Sulje
+        </button>
+      </Link>
+
+      <Link to="/mainMenu">
+        <button type="button">Palaa alkuun</button>
+      </Link>
     </div>
   );
 };
--- src/pages/MainMenuScreen.js
+++ src/pages/MainMenuScreen.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-import InfoDisplay from '../components/InfoDisplay';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const MainMenuScreen = () => {
+import InfoDisplay from '../components/InfoDisplay';
+import { removeUser } from '../reducers/userReducer';
+
+const MainMenuScreen = ({ removeUser }) => {
   const [welcomeMessage, setWelcomeMessage] = useState([]);
 
   const fetchInfo = async () => {
@@ -20,13 +25,38 @@ const MainMenuScreen = () => {
     fetchInfo();
   }, []);
 
+  const handleLogout = () => {
+    removeUser();
+  };
+
   return (
     <div>
       <h1>Tervetuloa, SUKUNIMI_TÄHÄN</h1>
 
       <InfoDisplay data={welcomeMessage} />
+
+      <Link to="/gameMenu">
+        <button type="button">Siirry peliin</button>
+      </Link>
+
+      <Link to="/">
+        <button type="button" onClick={handleLogout}>
+          Kirjaudu ulos
+        </button>
+      </Link>
     </div>
   );
 };
 
-export default MainMenuScreen;
+MainMenuScreen.propTypes = {
+  removeUser: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = {
+  removeUser,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(MainMenuScreen);
