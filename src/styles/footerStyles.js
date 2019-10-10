92bd168f37988985d84fa6ff2205f7a36341d3cc
Katri Henttonen <katri.henttonen@student.lamk.fi>
2019-10-10 11:21:35 +0300

    add cssbase

---
+++ src/styles/footerStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const styledCopyright = styled.p`
+  color: #a7a7a7;
+  font-size: 12px;
+`;
---
+++ src/styles/indexStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+    background-color: #E6E6E6;
+	font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", Verdana, "sans-serif";
+	color: white;
+	margin-top: 60px;
+}
+
+
+`;
